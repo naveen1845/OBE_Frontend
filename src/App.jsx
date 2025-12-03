b3339631d5166dcb223242dd5e7570a88c2d0d8a
NaveenPattihal <naveenpattihal08@gmail.com>
2025-12-03 17:56:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl bg-blue-600 text-white'>OBE Frontend</h1>
+      <p className='bg-blue-300 text-white'>- used tailwind here, plz continue your work from here</p>
+    </>
+  )
+}
+
+export default App
